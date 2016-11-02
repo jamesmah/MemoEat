daa97ad1142f35b3209bf979f44f259e93249dc5
James Mah <junyeumah@gmail.com>
2016-11-02 13:03:06 +1100

    added user details edit and add restaurant page. refactoring code

--- main.rb
+++ main.rb
@@ -23,9 +23,13 @@ end
 
 
 get '/' do
-  redirect to '/home' if logged_in?
-  
+  redirect to '/' + current_user['username'] if logged_in?
+
   erb :login
+end
+
+get '/home' do
+  redirect to '/'
 end
 
 get '/login' do
@@ -38,10 +42,10 @@ get '/signup' do
   erb :signup
 end
 
-patch '/signup' do
+post '/signup' do
   if !!User.find_by(username: params[:username])
     # do if user already exists
-    redirect to 'http://google.com'
+    redirect to '/signup'
   end
 
   user = User.new
@@ -54,9 +58,37 @@ patch '/signup' do
 
   if user.save
     session[:user_id] = user.id
-    redirect to '/home'
+    redirect to '/' + current_user['username']
   else
     redirect to '/signup'
+  end
+end
+
+get '/edituser' do
+  @user = User.find_by(id: session[:user_id])
+  erb :edituser, :layout => :layout_userpages
+end
+
+patch '/edituser' do
+  user = User.find_by(id: session[:user_id])
+  if !user
+    # do if user already exists
+    redirect to 'edituser'
+  end
+
+  user.name = params[:name]
+  user.username = params[:username]
+  user.email = params[:email]
+  if !!params[:password]
+    user.password_digest = BCrypt::Password.create(params[:password])
+  end
+  user.location_id = 259
+  user.description = params[:description]
+
+  if user.save
+    redirect to '/edituser'
+  else
+    redirect to '/edituser'
   end
 end
 
@@ -65,7 +97,7 @@ post '/session' do
   if user && user.authenticate(params[:password])
 
     session[:user_id] = user.id
-    redirect to '/home'
+    redirect to '/' + current_user['username']
   else
     redirect to '/'
   end
@@ -78,38 +110,76 @@ delete '/session' do
 end
 
 
-get '/home' do
-  redirect to '/' unless logged_in?
-
-  @restaurants = Restaurant.where(user_id: session[:user_id], archive: false).order("id ASC")
-
-  erb :home, :layout => :layout_userpages
-end
+# home directory
 
 delete '/home/:restaurant_id' do
   Restaurant.find_by(id: params[:restaurant_id]).destroy
-  redirect to '/home'
+  redirect to '/' + current_user['username']
 end
 
 patch '/home/archive/:restaurant_id' do
   restaurant = Restaurant.find_by(id: params[:restaurant_id])
   restaurant['archive'] = true
   restaurant.save
-  redirect to '/home'
-end
-
-patch '/home/unarchive/:restaurant_id' do
-  restaurant = Restaurant.find_by(id: params[:restaurant_id])
-  restaurant['archive'] = false
-  restaurant.save
-  redirect to '/history'
+  redirect to '/' + current_user['username']
 end
 
 patch '/home/notes/:restaurant_id' do
   restaurant = Restaurant.find_by(id: params[:restaurant_id])
   restaurant['notes'] = params[:notes]
   restaurant.save
-  redirect to '/home'
+  redirect to '/' + current_user['username']
+end
+
+patch '/home/rating/:restaurant_id' do
+  restaurant = Restaurant.find_by(id: params[:restaurant_id])
+  if restaurant['rating'] == params[:rating].to_i
+    restaurant['rating'] = 0
+  else
+    restaurant['rating'] = params[:rating]
+  end
+  restaurant.save
+  redirect to '/' + current_user['username']
+end
+
+
+# history directory
+get '/history' do
+  redirect to '/' unless logged_in?
+
+  @restaurants = Restaurant.where(user_id: session[:user_id], archive: true).order("id ASC")
+
+  erb :history, :layout => :layout_userpages
+end
+
+delete '/history/:restaurant_id' do
+  Restaurant.find_by(id: params[:restaurant_id]).destroy
+  redirect to '/history'
+end
+
+patch '/history/unarchive/:restaurant_id' do
+  restaurant = Restaurant.find_by(id: params[:restaurant_id])
+  restaurant['archive'] = false
+  restaurant.save
+  redirect to '/history'
+end
+
+patch '/history/notes/:restaurant_id' do
+  restaurant = Restaurant.find_by(id: params[:restaurant_id])
+  restaurant['notes'] = params[:notes]
+  restaurant.save
+  redirect to '/history'
+end
+
+patch '/history/rating/:restaurant_id' do
+  restaurant = Restaurant.find_by(id: params[:restaurant_id])
+  if restaurant['rating'] == params[:rating].to_i
+    restaurant['rating'] = 0
+  else
+    restaurant['rating'] = params[:rating]
+  end
+  restaurant.save
+  redirect to '/history'
 end
 
 get '/search' do
@@ -137,156 +207,45 @@ get '/search' do
   erb :search, :layout => :layout_userpages
 end
 
+
 post '/addnew' do
   restaurant = Restaurant.new
   restaurant.user_id = session[:user_id]
   restaurant.zomato_id = params[:zomato_id]
   restaurant.name = params[:name]
   restaurant.address = params[:address]
-  restaurant.cuisine = params[:cuisines]
+  restaurant.cuisines = params[:cuisines]
   restaurant.price_range = params[:price_range]
   restaurant.photo_url = params[:photo_url]
+  if !restaurant.rating = params[:rating]
+    restaurant.rating = 0
+  end
+  restaurant.notes = params[:notes]
   restaurant.archive = false
 
   restaurant.save
   # binding.pry
 
   # redirect to "/search?q=#{params[:q]}"
-  redirect to "/home"
+  redirect to '/' + current_user['username']
 end
 
-
-get '/history' do
-  redirect to '/' unless logged_in?
-
-  @restaurants = Restaurant.where(user_id: session[:user_id], archive: true).order("id ASC")
-
-  erb :history, :layout => :layout_userpages
+get '/add' do
+  erb :add, :layout => :layout_userpages
 end
 
-get '/newmemo' do
-  erb :newmemo, :layout => :layout_userpages
+# get '/:username/edituser' do
+#   erb :edituser, :layout => :layout_userpages
+# end
+
+get '/:username' do
+  if (!!@user = User.find_by(username: params[:username]) ) && logged_in?
+    @restaurants = Restaurant.where(user_id: @user['id'], archive: false).order("id ASC")
+
+    if logged_in?
+      erb :home, :layout => :layout_userpages
+    else
+      erb :home
+    end
+  end
 end
-
-get '/edituser' do
-  erb :edituser, :layout => :layout_userpages
-end
-
-
-# result = HTTParty.get('http://omdbapi.com/?t=once')
-# class Hash
-#   def downcase_key
-#     keys.each do |k|
-#       store(k.downcase, Array === (v = delete(k)) ? v.map(&:downcase_key) : v)
-#     end
-#     self
-#   end
-# end
-
-
-# get '/' do
-#   erb :index
-# end
-
-# get '/results' do
-#   @param = URI.escape(params[:title])
-#   @movie = {}
-#   @poster = nil
-
-#   open('search_history.txt', 'a') { |f|
-#     f.patchs @param
-#   }
-#   if @param == ''
-#     redirect "/about?id=#{@param}"
-#   else 
-#     @movie = HTTParty.get('http://omdbapi.com/?s=' + @param)
-#     if @movie['Response'] == 'False'
-#       redirect "/about?id=#{@param}"
-#     elsif @movie['Search'].length == 1
-#       imdbID = @movie['Search'][0]['imdbID']
-#       redirect "/about?id=#{imdbID}"
-#     else
-#       @moviearray = @movie['Search']
-#     end
-#   end
-#   erb :results
-# end
-
-
-# get '/about' do
-#   @param = URI.escape(params[:id])
-#   @movie = {}
-#   @poster = nil
-
-#   if @param == ''
-#     @result = 'No movie found'
-#   else 
-#     if Movie.find_by(imdbid: @param) != nil
-#       @movie = Movie.find_by(imdbid: @param).attributes
-#     else
-#       @movie = HTTParty.get('http://omdbapi.com/?i=' + @param)
-#       @movie.downcase_key.delete('type')
-#       if @movie['response'] == 'True'
-#         Movie.create(@movie)
-#       end
-#     end
-#     if @movie['response'] == 'False'
-#       @result = 'No movie found'
-#     end
-#   end
-#   erb :about
-# end
-
-# get '/history' do
-
-#   @newarray = []
-
-#   f = File.open("search_history.txt", "r")
-#   f.each_line do |line|
-#      @newarray << line
-#   end
-#   f.close
-
-#   erb :history
-# end
-
-
-
-
-
-
-
-
-
-# require 'bcrypt'
-
-# class User < ActiveRecord::Base
-#   # users.password_hash in the database is a :string
-#   include BCrypt
-
-#   def password
-#     @password ||= Password.new(password_hash)
-#   end
-
-#   def password=(new_password)
-#     @password = Password.create(new_password)
-#     self.password_hash = @password
-#   end
-# end
-# Creating an account
-
-# def create
-#   @user = User.new(params[:user])
-#   @user.password = params[:password]
-#   @user.save!
-# end
-# Authenticating a user
-
-# def login
-#   @user = User.find_by_email(params[:email])
-#   if @user.password == params[:password]
-#     give_token
-#   else
-#     redirect_to home_url
-#   end
-# end
--- models/restaurant.rb
+++ models/restaurant.rb
@@ -1,3 +1,4 @@
 class Restaurant < ActiveRecord::Base
   validates :name, length: { minimum: 2 };
+  validates :rating, length: { minimum: 1 };
 end
