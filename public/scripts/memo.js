983e215458f8404699b34fdf78f8ffd3bae46d02
James Mah <junyeumah@gmail.com>
2016-11-03 19:42:53 +1100

    fixed cursor issue

--- public/scripts/memo.js
+++ public/scripts/memo.js
@@ -7,7 +7,7 @@ if ($(window).width() < 768 && $(window).width() > 700) {
     $( "div.memo-collapse" ).removeClass("in");
     $( "div.panel-heading").attr("data-toggle", "collapse");
 }
-else if ($(window).width() < 768) {
+if ($(window).width() < 768) {
     $( "div.panel-heading" ).mouseenter( function() {
         document.body.style.cursor = "pointer";
     } ).mouseleave( function() {
